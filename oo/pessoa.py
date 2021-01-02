35888ccb845d8766dcd4862d48a79dc191ea1c47
Gabriel Igor <gabriel.igorq@gmail.com>
2021-01-02 08:24:15 -0300

    Criado método cumprimentar e atributos nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,18 @@
 class Pessoa:
-    pass
+    def __init__(self, nome = None, idade = 26):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('Gabriel')
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Igor'
+    print(p.nome)
+    print(p.idade)
