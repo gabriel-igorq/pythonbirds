9ff313f45caf4150719e14537183c3e981df230b
Gabriel Igor <gabriel.igorq@gmail.com>
2021-01-02 08:42:41 -0300

    Criado atributo de classe 'olhos'

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,5 +1,9 @@
 class Pessoa:
+    # Atributos de classe/default
+    olhos = 2
+
     def __init__(self, *filhos, nome = None, idade = 26):
+        # Atributos de instancia
         self.nome = nome
         self.idade = idade
         self.filhos = list(filhos)
@@ -20,5 +24,12 @@ if __name__ == '__main__':
         print(filho.nome)
     rogerin.sobrenome = 'Costa'
     del rogerin.filhos
+    rogerin.olhos = 1
+    del rogerin.olhos
     print(rogerin.__dict__)
     print(biel.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(rogerin.olhos)
+    print(biel.olhos)
+    print(id(Pessoa.olhos), id(rogerin.olhos), id(biel.olhos))
