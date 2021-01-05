b782103cbb7c141205b0bad5ec2488b2723d34f5
Gabriel Igor <gabriel.igorq@gmail.com>
2021-01-05 08:53:06 -0300

    Finalizado o jogo

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -10,7 +10,7 @@ class Pessoa:
 
     # Método da instância
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     # Método da classe(usando decorator)
     @staticmethod
@@ -22,10 +22,18 @@ class Pessoa:
     def nome_e_atributos_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe = super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+
+
+class Mutante(Pessoa):
+    olhos = 3
 
 if __name__ == '__main__':
-    biel = Pessoa(nome='Gabriel')
-    rogerin = Pessoa(biel, nome='Rogério')
+    biel = Mutante(nome='Gabriel')
+    rogerin = Homem(biel, nome='Rogério')
     print(Pessoa.cumprimentar(rogerin))
     print(id(rogerin))
     print(rogerin.cumprimentar())
@@ -39,10 +47,17 @@ if __name__ == '__main__':
     del rogerin.olhos
     print(rogerin.__dict__)
     print(biel.__dict__)
-    Pessoa.olhos = 3
     print(Pessoa.olhos)
     print(rogerin.olhos)
     print(biel.olhos)
     print(id(Pessoa.olhos), id(rogerin.olhos), id(biel.olhos))
     print(Pessoa.metodo_estatico(), rogerin.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_classe(), rogerin.nome_e_atributos_de_classe())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(biel, Homem))
+    print(isinstance(biel, Pessoa))
+    print(biel.olhos)
+    print(rogerin.cumprimentar())
+    print(biel.cumprimentar())
